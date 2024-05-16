276ce7a64655484eb1d0cdce3c58ee964bdac6ab
julietaper <jpercivale13@gmail.com>
2024-05-16 19:48:54 -0300

    format validations added

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -1,4 +1,6 @@
 class Patient < ApplicationRecord
     validates :name, :email, :phone_number, :document_photo, presence: true
-    validates :phone_number, format: { with: /\A\d{10}\z/, message: "debe tener 10 dígitos" }
-  end
+    validates :phone_number, format: { with: /\A\d{9}\z/, message: "phone must contain 9 digits" }    
+    validates :name, format: { with: /\A[a-zA-Z\s]+\z/, message: "special characters are not allowed" }
+    validates :email, format: { with: URI::MailTo::EMAIL_REGEXP, message: "Email must have a valid format" }
+end
